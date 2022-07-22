af2a4466d47a010839f968a0abd0adc53e378cf4
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-07-22 19:06:43 +0100

    Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Articulat CF", ...fontFamily.sans],
+      },
       keyframes: {
         // Navgiation menu
         "enter-from-right": {
