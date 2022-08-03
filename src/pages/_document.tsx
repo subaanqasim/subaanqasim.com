c76a7256f901a62d7abca13f3fd5f50b8ccc5a55
Subaan Qasim <subaan.qasim@gmail.com>
2022-08-03 16:25:33 +0100

    Add manifest metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,40 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/zqu1jvs.css" />
+        <link rel="manifest" href="/static/subaanqasim.webmanifest" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/icons/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/static/icons/safari-pinned-tab.svg"
+          color="#2e2e2e"
+        />
+        <link rel="shortcut icon" href="/static/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#171717" />
+        <meta
+          name="msapplication-config"
+          content="/static/icons/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#171717" />
+        <meta
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+          name="robots"
+        />
       </Head>
       <body>
         <Main />
