ef1b6c0c05b9e4f69bc95c0cbcae85b6b2e50ca5
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-08-09 22:56:46 +0100

    Change email auth to client side (#20)
    Server side email auth by calling own API endpoint is a pain to get working (if even possible), so changed to client side email auth flow.

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import { cda } from "@utils/contentful";
 import { InferGetStaticPropsType } from "next";
 import Wrapper from "../components/Wrapper";
 import Development from "../components/Development";
 import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import cx from "classnames";
 
 const Support = ({
   bannerImage,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [inputEmail, setInputEmail] = useState("");
   const { query } = useRouter();
 
   const isSuccess = query.success === "true";
+  const userEmail = query.email;
 
-  // TODO: DO SOME SESSION-BASED RENDERING -> MAKE SURE TO CROSS CHECK EMAIL/SESSION IN DB TO MAKE SURE IT HAS STRIPE ID -> I.E. they have donated, because someone could just hit the email auth endpoint manually without donating
+  const { data: session } = useSession();
 
   return (
     <Wrapper
@@ -20,13 +24,62 @@ const Support = ({
       description="Kind words from donations and supporters."
       image={bannerImage}
     >
-      {!isSuccess && <Development title="Support" />}
-      {isSuccess && (
-        <>
-          <h1>thanks my dude</h1>
-          <p>pls check ur email to sign-in and leave a message :)</p>
-        </>
+      <>
+        <header>
+          <h1>Support</h1>
+          {isSuccess && <h3 className="mt-12">thanks my dude 🥹</h3>}
+          {!session && (
+            <p className="max-w-prose mt-3">
+              Sign in using the email you used to tip to leave a message!
+              (entirely optional ofc)
+            </p>
+          )}
+        </header>
+      </>
+
+      {!session && (
+        <form
+          className="w-full mt-10"
+          onSubmit={(e) => {
+            e.preventDefault();
+            signIn("email", { email: userEmail ? userEmail : inputEmail });
+          }}
+        >
+          {!userEmail && (
+            <div className="max-w-sm mx-auto">
+              <label htmlFor="email" className="font-medium text-lg">
+                Email
+              </label>
+              <input
+                name="email"
+                id="email"
+                type="email"
+                value={inputEmail}
+                onChange={(e) => setInputEmail(e.target.value)}
+                placeholder="tim@apple.com"
+                className={cx(
+                  "block w-full rounded-md py-2 px-4 mt-1",
+                  "placeholder:text-neutral-500 dark:placeholder:text-neutral-600 bg-neutral-100",
+                  "border border-neutral-300 dark:border-neutral-700 dark:bg-neutral-900",
+                  "focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-white dark:focus-visible:ring-offset-black",
+                )}
+              />
+            </div>
+          )}
+          <div className="max-w-sm mx-auto mt-4">
+            <button className="button-primary w-full" type="submit">
+              Sign in with email
+            </button>
+          </div>
+        </form>
       )}
+
+      <p className="text-neutral-700 dark:text-neutral-300 mt-24 mx-auto text-center font-semibold">
+        Message board functionality coming soon!!
+      </p>
+      <p className="text-neutral-500 dark:text-neutral-400 mt-4 mx-auto text-center">
+        ur support is unbeaten 👉🏽👈🏽
+      </p>
     </Wrapper>
   );
 };
