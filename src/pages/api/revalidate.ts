d99c8fb85f7da83d7db3fac513ad5bbf6db4ff44
Subaan Qasim <subaan.qasim@gmail.com>
2022-09-30 22:05:54 +0100

    log errors

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -53,6 +53,8 @@ export default async function handler(
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
+    console.error(err);
+
     return res.status(500).send("Error revalidating");
   }
 }
