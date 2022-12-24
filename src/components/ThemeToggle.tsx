acfdcf335e74cf9e571551a1dec9e9dfe33d4680
Subaan Qasim <subaan.qasim@gmail.com>
2022-12-24 18:14:41 +0000

    add microanimation to theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { useTheme } from "next-themes";
 import { SunIcon, MoonIcon } from "./Icons";
-import { useHasMounted } from "@utils/hooks";
+import { useBounce, useHasMounted } from "@utils/hooks";
+import { animated } from "@react-spring/web";
 
 export default function ThemeToggle() {
   const hasMounted = useHasMounted();
   const { resolvedTheme, setTheme } = useTheme();
+
+  const { appliedStyle, trigger } = useBounce({
+    scale: 0.9,
+    rotation: resolvedTheme === "dark" ? 8 : -8,
+  });
 
   useEffect(() => {
     const onKeydown = (event: any) => {
@@ -22,17 +28,25 @@ export default function ThemeToggle() {
   }, [resolvedTheme, setTheme]);
 
   return (
-    <button
-      className="group rounded-full bg-white/50 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-neutral-800/50 dark:ring-white/10 dark:hover:ring-white/20"
-      aria-label="Toggle theme"
-      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    <animated.div
+      style={appliedStyle}
+      onMouseDown={trigger}
+      className="hover:scale-110"
     >
-      {hasMounted && (
-        <>
-          <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
-          <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
-        </>
-      )}
-    </button>
+      <button
+        className="group rounded-full bg-white/50 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition hover:scale-110 dark:bg-neutral-800/50 dark:ring-white/10 dark:hover:ring-white/20"
+        aria-label="Toggle theme"
+        onClick={() => {
+          setTheme(resolvedTheme === "dark" ? "light" : "dark");
+        }}
+      >
+        {hasMounted && (
+          <>
+            <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
+            <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
+          </>
+        )}
+      </button>
+    </animated.div>
   );
 }
